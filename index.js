e4bb07d08664549a2710ba9e011dd04215f19420
Wojciech Błachut <38980771+wblachut@users.noreply.github.com>
2020-12-15 17:50:06 +0100

    add functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const weatherApp = {}
+const applyLocationBtn = document.querySelector('.apply-loc-btn')
 const cityInput = document.getElementById('city-in')
 const searchBtn = document.querySelector('.search-btn')
+const locationDisplay = document.querySelector('.location-txt')
 const weatherDisplayDiv = document.querySelector('.weather-display')
 const weatherImg = weatherDisplayDiv.querySelector('img')
 const weatherDescription = document.querySelector('.weather-descript')
@@ -9,61 +11,121 @@ const tempSensedDisplay = document.querySelector('.sensed-temp')
 const pressureDisplay = document.querySelector('.pressure')
 const humidityDisplay = document.querySelector('.humidity')
 const windDisplay = document.querySelector('.wind')
-const windDirDisplay = document.querySelector('.wind-direction')
+const windDirImg = document.querySelector('.wind-direction')
+const tempUnitElements = document.querySelectorAll('.temp-unit')
 const tempUnitToggle = document.querySelector('.temp-toggle')
-let Celsius = ' ºC'
-let Fahrenheit = ' F'
-let tempUnit = Celsius;
+const citySlide = document.querySelector('.city-slide')
+
+let celsius = ' ºC'
+let fahrenheit = ' F'
+let tempUnit = celsius;
+
+const celsiusToFahrenheit = celsius => celsius * 9/5 + 32;
+const fahrenheitToCelsius = fahrenheit => (fahrenheit - 32) * 5/9;
+
 
 async function getWeather(city = 'Krakow') {
   try {
     const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},pl&units=metric&APPID=3a9bfc72412a8f1e5528cd7b61cf524d`, {mode: 'cors'});
     const weatherData = await response.json();
-    const imgId = await weatherData.weather[0].icon;
-    weatherImg.src = `http://openweathermap.org/img/wn/${imgId}@2x.png`
-    // img.src = catData.data.images.original.url;
     handleWeatherApp(weatherData);
   }
   catch {
-    return console.error(err);
+    (function (err) {
+      console.log(err);
+    });
   }
 }
 
-getWeather();
+// getWeather();
+getWeatherByLocation();
+// handleTempUnit();
 // console.log('script')
 
 function handleWeatherApp(data) {
   console.log(data);
-// cityDisplay.innerHTML = cityInput.value
-// weatherImg.src = data.weather[0].icon;
-weatherDescription.innerHTML = data.weather[0].main + ', ' + (data.weather[0].description);
-tempDisplay.innerHTML = data.main.temp + tempUnit;
-tempSensedDisplay.innerHTML = data.main.feels_like + tempUnit;
-pressureDisplay.innerHTML = data.main.pressure + ' hPa';
-humidityDisplay.innerHTML = data.main.humidity + ' %';
-windDisplay.innerHTML = data.wind.speed + ' m/s, ';
-windDirDisplay.innerHTML = data.wind.deg;
+  const imgId = data.weather[0].icon;
+  weatherImg.src = `http://openweathermap.org/img/wn/${imgId}@2x.png`
+  weatherDescription.innerHTML = data.weather[0].main + ', ' + (data.weather[0].description);
+  tempDisplay.innerHTML = Math.round(data.main.temp);
+  tempSensedDisplay.innerHTML = Math.round(data.main.feels_like);
+  pressureDisplay.innerHTML = data.main.pressure + ' hPa';
+  humidityDisplay.innerHTML = data.main.humidity + ' %';
+  windDisplay.innerHTML = data.wind.speed + ' m/s, ';
+  windDisplayedDeg = data.wind.deg - 90;
+  windDirImg.style.transform = `rotate(${windDisplayedDeg}deg)`;
+  locationDisplay.innerHTML = `${data.name}, ${data.sys.country}`;
 
   console.log(data.weather[0].icon);
-  console.log(data.main.feels_like);
-  console.log(data.main.temp);
-  console.log(data.main.humidity);
-  console.log(data.main.pressure);
-  console.log(data.main.temp_min);
-  console.log(data.main.temp_max);
+  console.log(data.main);
   console.log(data.weather[0].main);
   console.log(data.weather[0].description);
-  console.log(data.wind.speed);
-  console.log(data.wind.deg);
+  console.log(data.wind);
+  console.log(data.name, data.sys.country);
+  console.log(data.clouds.all);
 
+}
+
+function getWeatherByLocation() {
+  navigator.geolocation.getCurrentPosition(applyWeatherFromLocation, console.error('cannot get user location'))
+
+  function applyWeatherFromLocation(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log(position.coords)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&&units=metric&APPID=3a9bfc72412a8f1e5528cd7b61cf524d`)
+      .then(response => response.json())
+      .then(data => handleWeatherApp(data));
+  }
 }
 
 function handleTempUnit() {
-  if (tempUnitToggle.checked === true) {
-    tempUnit = Fahrenheit;
-  } else {
-    tempUnit = Celsius;
+  console.log('temp')
+  // if (tempUnit === celsius) {
+  //   tempUnit = fahrenheit;
+
+  //   // handleWeatherApp(data);
+  // } else {
+  //   tempUnit = celsius;
+  //   // handleWeatherApp(data);
+  // }
+  tempUnitElements[0].innerHTML = tempUnit;
+  tempUnitElements[1].innerHTML = tempUnit;
+}
+
+citySlide.addEventListener('click',() => { enableCityInput()});
+applyLocationBtn.addEventListener('click', () => {
+  getWeatherByLocation();
+  disableCityInput();
+});
+searchBtn.addEventListener('click', () => { getWeather(cityInput.value)});
+tempUnitToggle.addEventListener('change', handleTempUnit());
+
+cityInput.addEventListener('keyup', function handleEnterLookup(e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    getWeather(cityInput.value);  
+  }
+  if (cityInput.value === '' && cityInput.focus === false) {
+    setInterval(disableCityInput(), 1000);
+  }
+});
+
+
+
+
+
+function enableCityInput() {
+  console.log('show input')
+  cityInput.classList.add('active');
+  cityInput.focus();
+  citySlide.classList.remove('active');
+}
+
+function disableCityInput() {
+  if (cityInput.value === '' ) {
+  cityInput.value = '';
+  cityInput.classList.remove('active');
+  citySlide.classList.add('active');
   }
 }
-searchBtn.addEventListener('click', () => { getWeather(cityInput.value)})
-
